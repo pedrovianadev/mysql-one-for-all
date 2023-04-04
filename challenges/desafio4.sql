1824d9a1f795a9356582935ccdf0e042c897611e
Pedro Viana <pedroviana.developer13@gmail.com>
2023-04-04 18:24:02 -0300

    challenge 4

---
+++ challenges/desafio4.sql
@@ -0,0 +1,10 @@
+SELECT
+    P.pessoa_nome AS pessoa_usuaria,
+        IF(MAX(YEAR(historico_data_reproducao)) >= 2021,
+        'Ativa',
+        'Inativa'
+        ) AS status_pessoa_usuaria
+FROM SpotifyClone.pessoa AS P
+JOIN SpotifyClone.historico AS H ON H.pessoa_id = P.pessoa_id
+GROUP BY P.pessoa_nome
+ORDER BY P.pessoa_nome;
